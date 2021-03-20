3aa0d11f4c387d6a755c5c905014061b73bb33e2
Palash Das <dpalash.new@gmail.com>
2021-03-21 03:15:41 +0530

    Use jsonify instead of hand cranked json.dumps, so that response comes with content type 'application/json'
    Signed-off-by: Palash Das <dpalash.new@gmail.com>

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-import json
-
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask.json import JSONEncoder
 from flask_injector import FlaskInjector
 from injector import inject
 
@@ -10,25 +9,32 @@ from repositories.ListingRepository import ListingRepository
 from repositories.SkuRepository import SkuRepository
 from repositories.UserRepository import UserRepository
 
+
+# TODO: Use dataclass instead, as JSONEncoder supports that
+class ObjectJSONEncoder(JSONEncoder):
+    default = vars
+
+
 app = Flask(__name__)
+app.json_encoder = ObjectJSONEncoder
 
 
 @inject
 @app.route('/api/domains', methods=['GET'])
 def get_domains(domainRepository: DomainRepository):
-    return json.dumps(domainRepository.get_domains(), default=vars)
+    return jsonify(domainRepository.get_domains())
 
 
 @inject
 @app.route('/api/sku', methods=['GET'])
 def get_sku(skuRepository: SkuRepository):
-    return json.dumps(skuRepository.get_all_sku(), default=vars)
+    return jsonify(skuRepository.get_all_sku())
 
 
 @inject
 @app.route('/api/sku/<sku_id>', methods=['GET'])
 def get_all_sku(skuRepository: SkuRepository, sku_id):
-    return json.dumps(skuRepository.get_sku(sku_id), default=vars)
+    return jsonify(skuRepository.get_sku(sku_id))
 
 
 @inject
@@ -36,24 +42,24 @@ def get_all_sku(skuRepository: SkuRepository, sku_id):
 def get_all_listings(listingRepository: ListingRepository):
     if 'creator_id' in request.args.keys():
         creator_id = request.args.get('creator_id')
-        return json.dumps(listingRepository.get_listing_by_creator(creator_id), default=vars)
+        return jsonify(listingRepository.get_listing_by_creator(creator_id))
     elif 'bidder_id' in request.args.keys():
         bidder_id = request.args.get('bidder_id')
-        return json.dumps(listingRepository.get_listing_by_bidder(bidder_id), default=vars)
+        return jsonify(listingRepository.get_listing_by_bidder(bidder_id))
     else:
-        return json.dumps(listingRepository.get_all_listings(), default=vars)
+        return jsonify(listingRepository.get_all_listings())
 
 
 @inject
 @app.route('/api/listing/<listing_id>', methods=['GET'])
 def get_listing_by_id(listingRepository: ListingRepository, listing_id):
-    return json.dumps(listingRepository.get_listing_by_id(listing_id), default=vars)
+    return jsonify(listingRepository.get_listing_by_id(listing_id))
 
 
 @inject
 @app.route('/api/user/<user_id>', methods=['GET'])
 def get_user(userRepository: UserRepository, user_id):
-    return json.dumps(userRepository.get_user(user_id), default=vars)
+    return jsonify(userRepository.get_user(user_id))
 
 
 FlaskInjector(app=app, modules=[configure])
