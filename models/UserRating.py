163f9aacd563a37472d07e5556bb8869805ffd2f
ritabrata <ritabrata.m@rapido.bike>
2021-03-21 21:09:02 +0530

    [Rito] Add domain id to user rating

--- models/UserRating.py
+++ models/UserRating.py
@@ -1,7 +1,8 @@
 class UserRating:
-    def __init__(self, rating_id, user_id, rating):
+    def __init__(self, rating_id, user_id, domain_id, rating):
         self.rating_id = rating_id
         self.user_id = user_id
+        self.domain_id = domain_id
         self.rating = rating
 
     def get_rating(self):
--- repositories/UserRepository.py
+++ repositories/UserRepository.py
@@ -1,3 +1,4 @@
+from constants.Domains import *
 from constants.Users import *
 from models.User import User
 from models.UserRating import UserRating
@@ -7,9 +8,9 @@ class UserRepository:
     def __init__(self):
         self.users = {
             user_1_id: User(user_1_id, "Ritabrata Moitra", [
-                UserRating("rating_id_1", user_1_id, 4),
-                UserRating("rating_id_1", user_1_id, 3),
-                UserRating("rating_id_1", user_1_id, 4)
+                UserRating("rating_id_1", user_1_id, GROCERIES_DOMAIN, 4),
+                UserRating("rating_id_1", user_1_id, GROCERIES_DOMAIN, 3),
+                UserRating("rating_id_1", user_1_id, CLASSIFIEDS_DOMAIN, 4)
             ]),
             user_2_id: User(user_2_id, "Palash Das", []),
             user_3_id: User(user_3_id, "Sabyasachi Nandy", [])
