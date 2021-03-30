aa8b8e61c961d06fd7b9ab7873ff2581abc7ea26
ritabrata <ritabrata.m@rapido.bike>
2021-03-30 21:30:30 +0530

    [Rito] Edit flight sku details to remove indigo branding

--- constants/Skus.py
+++ constants/Skus.py
@@ -11,7 +11,7 @@ SKU_HAIRCUT_ID = "sku_haircut"
 SKU_HAIRCUT_DESCRIPTION = "Professional Haircut by Habib's stylist"
 SKU_HAIRCUT = Sku(SKU_HAIRCUT_ID, SKU_HAIRCUT_NAME, CLASSIFIEDS_DOMAIN, SKU_HAIRCUT_DESCRIPTION)
 
-SKU_FLIGHT_NAME = 'Indigo Flight B2B'
-SKU_FLIGHT_ID = 'Indigo Flight B2B'
-SKU_FLIGHT_DESCRIPTION = 'Indigo Boeing 737 flight from New Delhi to Brussels. Accepting B2B bids.'
+SKU_FLIGHT_NAME = 'Business Class Flight Seat B2B'
+SKU_FLIGHT_ID = 'Business Class Flight Seat B2B'
+SKU_FLIGHT_DESCRIPTION = 'Boeing 737 flight from New Delhi to Brussels. Accepting B2B bids for business class seats.'
 SKU_FLIGHT = Sku(SKU_FLIGHT_ID, SKU_FLIGHT_NAME, TRAVEL_DOMAIN, SKU_FLIGHT_DESCRIPTION)
